746156b05c811da582d08e9c74819d876208145a
Cindy <cindy@microryza.com>
2013-05-23 00:43:53 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omraisl::Application.config.secret_token = 'bf7dcfadfc5ac50a74ec4f026bd268abe5bd13bee288f1eb83356818f674860c79de2a59267cb3f5cdfe37a85446b16e92704fe319dbc96654fe22f5a117dcdf'
