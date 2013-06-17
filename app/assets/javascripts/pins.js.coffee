ab71cf92be43ca77d0bf19088291d498802ccc90
Cindy Wu <ciindy.wu@gmail.com>
2013-06-17 17:34:58 +0300

    fix the image resizing

--- app/assets/javascripts/pins.js.coffee
+++ app/assets/javascripts/pins.js.coffee
@@ -4,5 +4,14 @@
 # http://js2coffee.org/
 
 jQuery ->
-	$('#pins').imagesLoaded ->
-		$('#pins').masonry itemSelector: ".box"
+    $('#pins').imagesLoaded ->
+        $('#pins').masonry itemSelector: ".box"
+
+    if $('.pagination').length
+        $(window).scroll ->
+            url = $('.pagination .next_page a').attr('href')
+            if url && $(window).scrollTop() > $(document).height() - $(window).height() - 50
+                # What to do at the bottom of the page
+                $('.pagination').text("Fetching more pins...")
+                $.getScript(url)
+            $(window).scroll()
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,14 +1,11 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description, :image, :image_remote_url
 
-  #validations
   validates :description, presence: true
   validates :user_id, presence: true
-  validates_attachment :image, presence: :true,
-  														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-  														 size: { less_than: 5.megabytes }
-
-  #relationships
+  validates_attachment :image, presence: true,
+                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                            size: { less_than: 5.megabytes }
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>"}
   
@@ -16,4 +13,5 @@ class Pin < ActiveRecord::Base
     self.image = URI.parse(url_value) unless url_value.blank?
     super
   end
+
 end
